eab994a0f94dccb7ea149d5799f267cb7469228a
zhuyawen <zhuyawen1983@126.com>
2014-10-25 17:21:47 +0800

    add wine

--- auto.sh
+++ auto.sh
@@ -156,7 +156,7 @@ libgl1-mesa-dev g++-multilib mingw32 tofrodos \
 python-markdown libxml2-utils xsltproc zlib1g-dev:i386 \
 android-tools-adb android-tools-fastboot libcloog-isl-dev \
 texinfo gcc-multilib schedtool libxml2-utils libxml2 \
-schedtool optipng pngcrush pngquant vim $PARAM
+schedtool optipng pngcrush pngquant vim wine $PARAM
 
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
